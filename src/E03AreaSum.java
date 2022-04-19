18d10c7b91b152e11182534b7c1d7b970734f5f8
kadun1 <kadunjj@gmail.com>
2022-04-20 00:19:27 +0900

    Doit 코딩테스트 구역합 - 개인풀이 완

---
+++ src/E03AreaSum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class E03AreaSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < m; i++) {
+            int first = sc.nextInt();
+            int second = sc.nextInt();
+            int sum = 0;
+            for (int j = first-1; j < second; j++) {
+                sum += arr[j];
+            }
+            System.out.println("sum = " + sum);
+        }
+
+    }
+}
