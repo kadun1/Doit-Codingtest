4629fb6bf761cee6907fd31dce6da181fbb65e04
kadun1 <kadunjj@gmail.com>
2022-04-23 00:10:40 +0900

    Doit 코딩테스트 나머지합 연구중

--- src/E04AreaSum2.java
+++ src/E04AreaSum2.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class E04AreaSum2 {
@@ -10,20 +9,36 @@ public class E04AreaSum2 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n][n];
+        int[][] arr = new int[n + 1][n + 1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            for (int j = 1; j <= n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int i = 0; i < m; i++) {
+        int[][] sumArr = new int[n + 1][n + 1];
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                //2차원 배열의 합배열을 구하는 핵심로직
+                sumArr[i][j] = sumArr[i][j-1] + sumArr[i-1][j] - sumArr[i-1][j-1] + arr[i][j];
+            }
         }
 
-        Arrays.stream(arr).forEach(ints -> Arrays.stream(ints).forEach(i-> System.out.println("i = " + i)));
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            //합계산 구간을 제외한 합계지점을 뺀 후 겹쳐서 빠진 지점을 더해준다...
+            System.out.println(
+                    sumArr[x2][y2] - sumArr[x2][y1-1] - sumArr[x1-1][y2] + sumArr[x1-1][y1-1]
+            );
+        }
+
         br.close();
     }
 }
