1ec7f2090935ef0ee9e355eb24fd5c826118521a
kadun1 <kadunjj@gmail.com>
2022-04-24 01:33:39 +0900

    Doit 코딩테스트 연속합 구하기(투포인트) - 배열을 직접 선언하지 않고 인덱스를 구성하여 구현

--- src/E05RemainSum.java
+++ src/E05RemainSum.java
@@ -15,12 +15,23 @@ public class E05RemainSum {
 
         for (int i = 0; i < N; i++) {
             int remainder = (int) (S[i] % M);
+            // 혼자서 나누어 떨어지는 경우를 더해준다.
             if(remainder==0) answer++;
+            //나머지를 인덱스로 하여 그 개수를 더한다.
             C[remainder]++;
         }
 
         for (int i = 0; i < M; i++) {
             if (C[i] > 1) {
+                /*
+                    combination(조합)의 공식 nCr
+                    nCr = nPr / r! 인데
+                    nPr = n! / (n-r)! 이므로
+                    nCr = n! / r!(n-r)! 이 된다.
+                    여기선 r이 2 이므로..(순서쌍)
+                    n! / (n-2)! = n * (n-1)
+                    그 후 순열이 아니라 조합이므로, r!(2)로 나누어준다.
+                 */
                 answer = answer + (C[i] * (C[i] - 1) / 2);
             }
         }
---
+++ src/E06ContinueSum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class E06ContinueSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 1;
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+
+        while (end_index != n) {
+            if (sum == n) {
+                end_index++;
+                sum += end_index;
+                count++;
+            } else if(sum > n){
+                sum -= start_index;
+                start_index++;
+            } else if(sum < n){
+                sum += end_index;
+                end_index++;
+            }
+        }
+        System.out.println("count = " + count);
+
+    }
+}
